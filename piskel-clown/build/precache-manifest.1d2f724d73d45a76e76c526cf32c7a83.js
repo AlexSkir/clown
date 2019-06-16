324bdff4431758860993c7aba8fa3a6e91a7d984
AlexSkir <a.skirnevskaia@gmail.com>
2019-06-16 18:11:15 +0300

    feat: deploying

---
+++ piskel-clown/build/precache-manifest.1d2f724d73d45a76e76c526cf32c7a83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca86dc93c339da3c2d79cb1eb9cf1e3",
+    "url": "/clown/piskel-clown/public/index.html"
+  },
+  {
+    "revision": "3ecc244d384332febb47",
+    "url": "/clown/piskel-clown/public/static/css/main.b4b04f11.chunk.css"
+  },
+  {
+    "revision": "4a67a10070bc67603902",
+    "url": "/clown/piskel-clown/public/static/js/2.dfdaf191.chunk.js"
+  },
+  {
+    "revision": "3ecc244d384332febb47",
+    "url": "/clown/piskel-clown/public/static/js/main.2af3f885.chunk.js"
+  },
+  {
+    "revision": "eaf1a9e75d52dce71ab3",
+    "url": "/clown/piskel-clown/public/static/js/runtime~main.2d4258b5.js"
+  }
+]);
