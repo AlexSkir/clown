a094c78b216f78095186fbc4d3b9d016562c496b
AlexSkir <a.skirnevskaia@gmail.com>
2020-03-20 13:45:06 +0300

    Updates

---
+++ precache-manifest.a706e64add3aed7a9e9d13b0f5a2a291.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c95036b6708f961412289bd4ceb0386",
+    "url": "/clown/piskel-clown/build/index.html"
+  },
+  {
+    "revision": "5fff008b11d24fb02b0c",
+    "url": "/clown/piskel-clown/build/static/css/main.a49ff90e.chunk.css"
+  },
+  {
+    "revision": "dc0d68b9639beb4bc9af",
+    "url": "/clown/piskel-clown/build/static/js/2.e52f9430.chunk.js"
+  },
+  {
+    "revision": "bd4e9a93e435bb3ee7dde8cbf12485d6",
+    "url": "/clown/piskel-clown/build/static/js/2.e52f9430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fff008b11d24fb02b0c",
+    "url": "/clown/piskel-clown/build/static/js/main.e3c0ee01.chunk.js"
+  },
+  {
+    "revision": "51be8d0e43da77bb14f5",
+    "url": "/clown/piskel-clown/build/static/js/runtime-main.f88889ed.js"
+  },
+  {
+    "revision": "d70d2a0b6bf6e0b25cdb27e307cfbf4b",
+    "url": "/clown/piskel-clown/build/static/media/error.d70d2a0b.jpg"
+  },
+  {
+    "revision": "ae1e58e5610122726451a4b100b3bcae",
+    "url": "/clown/piskel-clown/build/static/media/export-gif.ae1e58e5.png"
+  },
+  {
+    "revision": "aa901b9d5adaf13ef51a60d8f007a3b4",
+    "url": "/clown/piskel-clown/build/static/media/home.aa901b9d.png"
+  },
+  {
+    "revision": "635e8d33b1d5cdbd1be7009195512525",
+    "url": "/clown/piskel-clown/build/static/media/homepage.635e8d33.gif"
+  },
+  {
+    "revision": "e3ca5e51ebb788b5495c97bddc550f51",
+    "url": "/clown/piskel-clown/build/static/media/preview-example.e3ca5e51.gif"
+  },
+  {
+    "revision": "d4571089e81468a68739ebc979c395f7",
+    "url": "/clown/piskel-clown/build/static/media/private-page.d4571089.png"
+  }
+]);
